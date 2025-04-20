018ea9400a65ae6e5400ed90af24095b863dfa72
Lucky <goyallucky2020@gmail.com>
2025-04-20 14:52:22 +0530

    view map and hydraion error fixes

--- src/components/ThemeProvider/index.tsx
+++ src/components/ThemeProvider/index.tsx
@@ -1,11 +1,22 @@
-"use client"
+"use client";
 
-import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { ReactNode } from "react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { ReactNode, useEffect, useState } from "react";
 
-export function ThemeProvider({ children, ...props }: {
+export function ThemeProvider({
+  children,
+  ...props
+}: {
   children: ReactNode;
-  [key: string]: string | ReactNode;
+  [key: string]: any;
 }) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
