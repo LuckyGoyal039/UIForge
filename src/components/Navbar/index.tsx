c9d3652000ac4a1fa2dd23b9ecee82004ca09717
Lucky <goyallucky2020@gmail.com>
2025-06-20 10:50:07 +0530

    change logo and favicon

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,11 +1,21 @@
+import Image from "next/image";
 import { ModeToggle } from "../ModeToggle";
-
+import MainLogo from '@/assets/images/mainLogo.png';
 
 export function Navbar() {
   return (
-    <nav className="flex items-center justify-between p-4">
-      <div>Your Logo</div>
-      <ModeToggle/>
+    <nav className="sticky top-0 z-50 flex items-center justify-between px-4 py-2 mb-4 bg-white dark:bg-black shadow-md">
+      <div className="w-20 sm:w-24 md:w-28 lg:w-32">
+        <Image
+          src={MainLogo}
+          alt="Ui Forge"
+          layout="responsive"
+          width={50}
+          height={50}
+          priority
+        />
+      </div>
+      <ModeToggle />
     </nav>
-  )
+  );
 }
